6c8f137bc129960710cafeace7503f199c73b733
gaubee <gaubeebangeel@gmail.com>
2013-08-18 12:11:07 +0800

    优化属性绑定 确保属性处理函数的重用，不生成新的作用域，后期优化皆是采用这种方式进行优化
    Signed-off-by: gaubee <gaubeebangeel@gmail.com>

--- build/HTML-ViewParse.js
+++ build/HTML-ViewParse.js
@@ -335,7 +335,6 @@ function _buildHandler(handleNodeTree) {
 };
 var _attrRegExp = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g;
 var _isIE = !+"\v1";
-var _event_by_fun = true;
 //by RubyLouvre(司徒正美)
 //setAttribute bug:http://www.iefans.net/ie-setattribute-bug/
 var IEfix = {
@@ -364,7 +363,111 @@ var IEfix = {
 	valign: "vAlign",
 	vspace: "vSpace"
 };
-var _comment_reg = /<!--[\w\W]*?-->/g;
+/*
+The full list of boolean attributes in HTML 4.01 (and hence XHTML 1.0) is (with property names where they differ in case):
+
+checked             (input type=checkbox/radio)
+selected            (option)
+disabled            (input, textarea, button, select, option, optgroup)
+readonly            (input type=text/password, textarea)
+multiple            (select)
+ismap     isMap     (img, input type=image)
+
+defer               (script)
+declare             (object; never used)
+noresize  noResize  (frame)
+nowrap    noWrap    (td, th; deprecated)
+noshade   noShade   (hr; deprecated)
+compact             (ul, ol, dl, menu, dir; deprecated)
+*/
+
+//  key : [isboolean , (default boolean value)isreverse]
+var _Assignment = {
+	className: false,
+	value: false,
+	checked: [true, false],
+	selected: [true], // equal to [true,false]
+	disabled: [true],
+	readonly: [true],
+};
+
+var _testDIV = $.DOM.clone(shadowDIV);
+var _event_by_fun = (function() {
+	try {
+		var testEvent = Function(""),
+			attrKey = "onclick";
+		console.log
+		_testDIV.setAttribute(attrKey, testEvent);
+		if (typeof _testDIV.getAttribute(attrKey) === "string") {
+			return false;
+		}
+	} finally {
+		return true;
+	}
+}());
+var _booleanFalseRegExp = /false|undefined|null|NaN/;
+var AttributeHandle = function(attrKey) {
+	var assign;
+	if (attrKey === "style" && _isIE) {
+		return AttributeHandle.list.style;
+	}
+	if (attrKey.indexOf("on") === 0 && _event_by_fun) {
+		return AttributeHandle.list.event;
+	}
+	console.log(_Assignment, attrKey)
+	if (_hasOwn.call(_Assignment, attrKey)) {
+		if (assign = _Assignment[attrKey]) {
+			if (assign[1]) {
+				return AttributeHandle.list.reBool;
+			}
+			return AttributeHandle.list.bool;
+		}
+		return AttributeHandle.list.dir;
+	}
+	return AttributeHandle.list.com;
+
+};
+_getAttrOuter = Function("n", "return n." + (_hasOwn.call(shadowDIV, "innerText") ? "innerText" : "textContent") + "||''")
+AttributeHandle.list = {
+	event: function(key, currentNode, parserNode) {
+		var attrOuter = _getAttrOuter(parserNode);
+		try {
+			var attrOuterEvent = Function(attrOuter);
+		} catch (e) {
+			attrOuterEvent = $.noop;
+		}
+		currentNode.setAttribute(attrKey, attrOuterEvent);
+	},
+	style: function(key, currentNode, parserNode) {
+		var attrOuter = _getAttrOuter(parserNode);
+		currentNode.style.setAttribute('cssText', attrOuter);
+	},
+	com: function(key, currentNode, parserNode) {
+		var attrOuter = _getAttrOuter(parserNode);
+		currentNode.setAttribute(key, attrOuter);
+	},
+	dir: function(key, currentNode, parserNode) {
+		var attrOuter = _getAttrOuter(parserNode);
+		currentNode[key] = attrOuter;
+	},
+	bool: function(key, currentNode, parserNode) {
+		var attrOuter = $.trim(_getAttrOuter(parserNode).replace(_booleanFalseRegExp, ""));
+		if (attrOuter) {
+			currentNode.setAttribute(key, true);
+		} else {
+			currentNode.removeAttribute(key);
+		}
+	},
+	reBool: function(key, currentNode, parserNode) {
+		var attrOuter = $.trim(_getAttrOuter(parserNode).replace(_booleanFalseRegExp, ""));
+		if (!attrOuter) {
+			currentNode.setAttribute(key, true);
+		} else {
+			currentNode.removeAttribute(key);
+		}
+	}
+};
+// var _comment_reg = /<!--[\w\W]*?-->/g;
 
 function _buildTrigger(handleNodeTree, dataManager) {
 	var self = this, //View Instance
@@ -414,32 +517,16 @@ function _buildTrigger(handleNodeTree, dataManager) {
 						and lock it into attrViewInstance, 
 						waiting for updates the attribute.*/ //(so the trigger of be injecte in mush be unshift)
 						currentNode: null,
+						_bindHandle: AttributeHandle(attrKey),
 						bindHandle: function() {
 							var self = this,
 								currentNode = self.currentNode,
 								parserNode = self.parserNode,
 								attrOuter = parserNode.innerText || parserNode.textContent || "";
-							// console.log("using currentNode:",currentNode);
+							console.log(self._bindHandle)
 							if (currentNode) {
-								if (attrKey === "style" && _isIE) {
-									currentNode.style.setAttribute('cssText', attrOuter);
-								} else if (attrKey.indexOf("on") === 0 && _event_by_fun) {
-									try {
-										var attrOuterEvent = Function(attrOuter);
-									} catch (e) {
-										attrOuterEvent = $.noop;
-									}
-									currentNode.setAttribute(attrKey, attrOuterEvent);
-									if (typeof currentNode.getAttribute(attrKey) === "string") {
-										_event_by_fun = false;
-										currentNode.setAttribute(attrKey, attrOuter);
-									}
-								} else {
-									currentNode.setAttribute(attrKey, attrOuter);
-								}
-								if (attrKey === "value") {
-									currentNode.value = attrOuter;
-								}
+								self._bindHandle(attrKey, currentNode, parserNode);
+								return;
 							}
 						}
 					};
