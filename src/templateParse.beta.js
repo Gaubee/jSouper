c3c88cf87bb4e12d853bac6019c5aecbff772d01
Gaubee <GaubeeBangeel@gmail.com>
2013-09-02 23:37:42 +0800

    Signed-off-by: Gaubee <GaubeeBangeel@gmail.com>

--- src/templateParse.beta.js
+++ src/templateParse.beta.js
@@ -5,7 +5,8 @@ var matchReg = /\{\{([\w\W]+?)\}\}/g,
 	},
 	handleType = {
 		arg: {},
-		pre: {}
+		pre: {},
+		emp: {}
 	},
 	parse = function(str) {
 		result = str.replace(matchReg, function(matchStr, innerStr, index) {
@@ -13,12 +14,12 @@ var matchReg = /\{\{([\w\W]+?)\}\}/g,
 			var fun_name = innerStr.trim().split(" ")[0];
 			if (fun_name in handles) {
 				if (handles[fun_name]) {
-					return "{"+fun_name+"("+parseArg(innerStr.replace(fun_name,"").trim())+")}";
-				}else{
-					return "{"+fun_name+"()}";
+					return "{" + fun_name + "(" + parseArg(innerStr.replace(fun_name, "").trim()) + ")}";
+				} else {
+					return "{" + fun_name + "()}";
 				}
 			} else {
-				return "{("+innerStr+")}";//"{("+parseArg(innerStr)+")}";
+				return "{(" + innerStr + ")}"; //"{("+parseArg(innerStr)+")}";
 			}
 		})
 		return result;
@@ -27,19 +28,54 @@ var matchReg = /\{\{([\w\W]+?)\}\}/g,
 		var stack = [];
 		console.log(argStr);
 		var pointer = 0;
-		argStr.replace(/([\W]+?)/g,function(matchOperator,operator,index,str){
+		argStr.replace(/([\W]+?)/g, function(matchOperator, operator, index, str) {
 			// console.log(arguments)
-			stack.push(str.substring(pointer,index));
-			pointer+=index+matchOperator.length;
+			if (operator !== ".") {
+				stack.push({
+					type: "arg",
+					value: str.substring(pointer, index)
+				}, {
+					type: "ope",
+					value: operator
+				});
+				pointer = index + matchOperator.length;
+			}
+			return matchOperator;
 		});
-		console.log(stack)
+		if (argStr.length - pointer) {
+			stack.push({
+				type: "arg",
+				value: argStr.substring(pointer, argStr.length)
+			})
+		}
+		console.log(pointer, argStr.length)
+		// stack = argStr.split(/([\W]+?)/);
+		parseIte(stack);
+		console.log(stack);
 		return argStr;
 	},
-	registerHandle = function(handleName) {
-		handles[handleName] = true;
+	parseIte = function(arr) {
+		var status = handleType.emp;
+		arr.forEach(function(block, index) {
+			switch (block.type) {
+				case "arg":
+					if (status === handleType.pre) {
+
+					} else {
+						block.parse = "{(" + block.value + ")}";
+					}
+					break;
+				case "ope":
+					break;
+			}
+		});
+		return arr;
 	};
+registerHandle = function(handleName) {
+	handles[handleName] = true;
+};
 
 
 var testStr = "{{  #if bool}} {{name}} {{/if}}"; //==>{#if( {(bool)} )} {(name)} {/if()}
-var testStr = "{{  #if a+b}} {{name}} {{/if}}"; //==>{#if( {+({(a)}{(b)})} )} {(name)} {/if()}
+var testStr = "{{  #if a.x+b+c  }} {{name}} {{/if}}"; //==>{#if( {+({(a)}{(b)})} )} {(name)} {/if()}
 console.log(parse(testStr));
