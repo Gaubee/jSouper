478ea73db8a0f43710d0893d883db3f2bf932af5
Gaubee <GaubeeBangeel@gmail.com>
2013-09-29 22:36:08 +0800

    取消特殊前缀跨作用域访问对象，将使用AOP插件实现。
    Signed-off-by: Gaubee <GaubeeBangeel@gmail.com>

--- src/DataManager.v4.js
+++ src/DataManager.v4.js
@@ -24,14 +24,14 @@ function TriggerKeySet() {
 		$.p(currentCollection, value)
 	}
 };
-function TriggerKeyItem(key,dataManager){
+
+function TriggerKeyItem(key, dataManager) {
 	var self = this;
 	self.key = key;
 	self.dataManager = dataManager;
 };
 TriggerKeyItem.prototype = {
-	get:function(){
-	}
+	get: function() {}
 };
 (function() {
 	function DataManager(baseData) {
@@ -42,7 +42,7 @@ TriggerKeyItem.prototype = {
 		baseData = baseData || {};
 		self.id = $.uid();
 		self._database = baseData;
-		self._cacheData = {};
+		// self._cacheData = {};
 		self._viewInstances = []; //to touch off
 		self._parentDataManager // = $UNDEFINED; //to get data
 		self._prefix // = $NULL; //冒泡时需要加上的前缀
@@ -64,28 +64,101 @@ TriggerKeyItem.prototype = {
 		if (sObj instanceof Object && nObj instanceof Object) {
 			for (var i in nObj) {
 				obj_n = nObj[i];
-				if ((obj_s = sObj[i])._DM_extends_object_constructor) { //拓展的DM_Object对象，通过接口实现操作
+				obj_s = sObj[i];//||(sObj[i]={});
+				if (obj_s&&obj_s._DM_extends_object_constructor) { //拓展的DM_Object对象，通过接口实现操作
 					obj_s.set(obj_n);
 				} else if (obj_s !== obj_n) { //避免循环 Avoid Circular
 					sObj[i] = _mix(obj_s, obj_n);
 				}
-				DataManager.set(sObj, i, nObj);
+				// DataManager.set(sObj, i, nObj);
 			}
 			return sObj;
 		} else {
 			return nObj;
 		}
 	};
-	var DMconfig = DataManager.config = {
-		"THIS": "$THIS", // _placeholder(),
-		"PARENT": "$PARENT", // _placeholder(),
-		"TOP": "$TOP" // _placeholder()
-	};
 
 	// var direction = []; //direction.length>0 , from the parent node.
 	DataManager.prototype = {
-		get: function(key) {},
-		set: function(key, nObj) {},
+		get: function(key) { //
+			var self = this,
+				arrKey = key.split("."),
+				parent,
+				result = self._database;
+			if (result != $UNDEFINED && result !== $FALSE) { //null|undefined|false
+				do {
+					result = $.valueOf(result[arrKey.splice(0, 1)]);
+				} while (result !== $UNDEFINED && arrKey.length);
+			}
+			if (arrKey.length&&(parent = self._parentDataManager)) {//key不在对象中，查询父级
+				result = parent.get(key);
+			}
+			return result;
+		},
+		mix: function(key, nObj) {
+			//mix Data 合并数据
+			var self = this,
+				keys,
+				lastKey,
+				cache_top_n_obj,
+				cache_n_Obj;
+			switch (arguments.length) {
+				case 0:
+					break;
+				case 1:
+					nObj = key;
+					if (self._database !== nObj || nObj instanceof Object) {
+						self._database = _mix(self._database, nObj);
+					};
+					key = "";
+					break;
+				default:
+					var sObj = self.get(key)
+					if (sObj&&sObj[_DM_extends_object_constructor]) { //是DataManager.Object的拓展对象
+						sObj.set(nObj); //调用拓展对象的接口
+					} else {
+						keys = key.split(".");
+						lastKey = keys.pop();
+						cache_top_n_obj = cache_n_Obj = {};
+						$.ftE(keys, function(nodeKey) { //根据对象链生成可混合对象
+							cache_n_Obj = (cache_n_Obj[nodeKey] = {});
+						});
+						cache_n_Obj[lastKey] = nObj;
+						self._database = _mix(self._database, cache_top_n_obj);
+					}
+			}
+			return self.touchOff(key);
+		},
+		set: function(key, nObj) {
+			//replace Data 取代原有对象数据
+			var self = this,
+				keys,
+				lastKey;
+			switch (arguments.length) {
+				case 0:
+					break;
+				case 1:
+					nObj = key;
+					if (self._database !== nObj || nObj instanceof Object) {
+						self._database = nObj;
+					};
+					key = "";
+					break;
+				default:
+					var database = self._database||(self._database = {}),
+						cache_n_Obj = database,
+						arrKey = key.split("."),
+						lastKey = arrKey.pop();
+					$.ftE(arrKey,function(currentKey){
+						cache_n_Obj = cache_n_Obj[currentKey]||(cache_n_Obj[currentKey] = {})
+					});
+					cache_n_Obj[lastKey] = nObj;
+			}
+			return self.touchOff(key);
+		},
+		touchOff:function(key){
+
+		},
 		_touchOffSubset: function(key) {},
 		_collectTriKey: function(viewInstance) {},
 		collect: function(dataManager) { /*收集dataManager的触发集*/
@@ -102,8 +175,8 @@ TriggerKeyItem.prototype = {
 				dmTriggerKeys = dataManager._triggerKeys,
 				dotPrefix = prefix ? prefix += "." : ""
 			dataManager._prefix = prefix;
-			dataManager._parentDataManager&&dataManager._parentDataManager.remove(dataManager);
-			
+			dataManager._parentDataManager && dataManager._parentDataManager.remove(dataManager);
+
 			dmTriggerKeys.forIn(function(dmTriggerCollection, key) {
 				myTriggerKeys.push(dotPrefix + key, dmTriggerCollection);
 			});
