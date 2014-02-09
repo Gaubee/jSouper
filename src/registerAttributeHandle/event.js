40cdaae903f80f75f9ae296f7282adb466f8cbea
Gaubee <Gaubeebangeel@gmail.com>
2014-02-09 09:02:04 +0800

    修复事件绑定使用字符串的BUG
    事件绑定基于字符串，如果事件一开始没有给的话，后来set并不会触发单独字符串的更新。所以把事件的获取写入到触发内部。

--- src/registerAttributeHandle/event.js
+++ src/registerAttributeHandle/event.js
@@ -1,9 +1,8 @@
 var _elementCache = {},
-	eventListerAttribute = function(key, currentNode, parserNode, vi /*, dm_id*/ ) {
+	eventListerAttribute = function(key, currentNode, parserNode, vi /*, dm_id*/ ,handle, triggerTable) {
 		var attrOuter = _getAttrOuter(parserNode),
 			eventInfos = key.replace("event-", "").toLowerCase().split("-"),
 			eventName = eventInfos.shift(), //Multi-event binding
-			eventFun = vi.get(attrOuter) || $.noop, //can remove able
 			elementHashCode = $.hashCode(currentNode, "event" + eventInfos.join("-"));
 		if (eventName.indexOf("on") === 0) {
 			eventName = eventName.substr(2)
@@ -15,11 +14,14 @@ var _elementCache = {},
 		var wrapEventFun = eventCollection[eventName]
 		if (!wrapEventFun) { //init Event and register event
 			wrapEventFun = eventCollection[eventName] = function(e) {
-				return wrapEventFun.eventFun.call(this, e, vi)
+				//因为事件的绑定是传入事件所在的key，所以外部触发可能只是一个"."类型的字符串
+				//没法自动更新eventFun，只能自动更新eventName，因此eventFun要动态获取
+				var eventFun = vi.get(attrOuter) || $.noop;
+				return eventFun.call(this, e, vi)
 			}
 			_registerEvent(currentNode, eventName, wrapEventFun, elementHashCode);
 		}
-		wrapEventFun.eventFun = eventFun;
+		wrapEventFun.eventName = eventName;
 	};
 
 V.ra(function(attrKey) {
