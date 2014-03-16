65e656a5aa1926ec5b7cf20c9b244a70bcfd10da
Gaubee <Gaubeebangeel@gmail.com>
2014-03-16 12:51:16 +0800

    on-event属性的绑定改进

--- build/jSouper.debug.js
+++ build/jSouper.debug.js
@@ -3903,16 +3903,18 @@ V.rt("", function(handle, index, parentHandle) {
             trigger.key = key;
             trigger.event = function(NodeList_of_ViewModel, model, /* triggerBy,*/ isAttr /*, vi*/ ) { //call by ViewModel's Node
                 var data = model.get(key),
-                    nodeHandle = NodeList_of_ViewModel[textHandleId];
+                    nodeHandle = NodeList_of_ViewModel[textHandleId],
+                    currentNode = nodeHandle.currentNode;
+
                 if (isAttr) {
                     //字符串事件：IE浏览器直接编译，故不需要转义，其他浏览器需要以字符串绑定到属性中。需要转义，否则会出现引号冲突
-                    if (isAttr.key.indexOf("on") === 0 && !_isIE) {
+                    if (isAttr.key.indexOf("on") === 0 && currentNode.hasOwnProperty(isAttr.key) && !_isIE) {
                         data = String(data).replace(/"/g, '\\"').replace(/'/g, "\\'");
                     }
                 }
                 // data = String(data);
                 if (nodeHandle._data !== data) {
-                    nodeHandle._data = nodeHandle.currentNode.data = (data === $UNDEFINED ? "" : data);
+                    nodeHandle._data = currentNode.data = (data === $UNDEFINED ? "" : data);
                 }
             }
         }
